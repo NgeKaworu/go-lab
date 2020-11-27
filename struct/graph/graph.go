222cdeaba98d321fb45d4c2e637d0fe83627f98e
fuRan <NgeKaworu@gmail.com>
2020-11-27 16:33:26 +0800

    打印方法没问题, backtrack没问题

--- struct/graph/graph.go
+++ struct/graph/graph.go
@@ -1,5 +1,10 @@
 package graph
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // Graph 图结构
 type Graph struct {
 	start *Vertex
@@ -12,6 +17,75 @@ type Vertex struct {
 	out map[*Vertex]int // 出度
 }
 
+// NewVertex 工厂方法
+func NewVertex(k int) *Vertex {
+	return &Vertex{key: k, in: map[*Vertex]int{}, out: map[*Vertex]int{}}
+}
+
+// BFS 打印
+func (g *Graph) String() (res string) {
+	// 标记去过的地方
+	memo := map[*Vertex]bool{g.start: true}
+	queue := []*Vertex{g.start}
+	for len(queue) != 0 {
+		dequeue := queue[0]
+		queue = queue[1:]
+		for vertex, weight := range dequeue.out {
+			if memo[vertex] == false {
+				queue = append(queue, vertex)
+				memo[vertex] = true
+			}
+			res += fmt.Sprintf(" %v --(%v)--> %v ", dequeue.key, weight, vertex.key)
+		}
+		res += "\n"
+	}
+	return
+}
+
+// FindAllPath 返回所有路径 回溯实现
+func (v *Vertex) FindAllPath(end int) []string {
+	res := make([]string, 0)
+
+	var backtrack func(v *Vertex, memo map[*Vertex]bool)
+	backtrack = func(v *Vertex, memo map[*Vertex]bool) {
+		if v.key == end {
+			var tmp string
+			var pre *Vertex
+			for v := range memo {
+				if pre != nil {
+					tmp += fmt.Sprintf(" --(%v)--> ", pre.out[v])
+				}
+				pre = v
+				tmp += strconv.Itoa(v.key)
+			}
+			if pre != nil {
+				tmp += fmt.Sprintf(" --(%v)--> %v", pre.out[v], v.key)
+			}
+			res = append(res, tmp)
+			return
+		}
+
+		for k := range v.out {
+			if memo[k] {
+				continue
+			}
+			memo[v] = true
+			backtrack(k, memo)
+			delete(memo, v)
+		}
+
+	}
+
+	m := make(map[*Vertex]bool)
+	backtrack(v, m)
+	return res
+}
+
+// ShortestPath 返回所有路径
+func (v *Vertex) ShortestPath(end int) string {
+	return ""
+}
+
 /*
 Connect 连接两个顶点
 degree 度 —— 0: 出度; 1: 入度; 2:双向
@@ -21,13 +95,17 @@ func (v *Vertex) Connect(n *Vertex, degree, weight int) *Vertex {
 	switch degree {
 	case 0:
 		v.out[n] = weight
+		n.in[v] = weight
 		break
 	case 1:
 		v.in[n] = weight
+		n.out[v] = weight
 		break
 	case 2:
 		v.out[n] = weight
 		v.in[n] = weight
+		n.out[v] = weight
+		n.in[v] = weight
 		break
 
 	}
@@ -42,13 +120,17 @@ func (v *Vertex) Disconnect(n *Vertex, degree int) *Vertex {
 	switch degree {
 	case 0:
 		delete(v.out, n)
+		delete(n.in, v)
 		break
 	case 1:
 		delete(v.in, n)
+		delete(n.out, v)
 		break
 	case 2:
 		delete(v.out, n)
+		delete(n.out, v)
 		delete(v.in, n)
+		delete(n.in, v)
 		break
 
 	}
--- struct/graph/graph_test.go
+++ struct/graph/graph_test.go
@@ -1,7 +1,31 @@
 package graph
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGraph(t *testing.T) {
+	v5 := NewVertex(5)
+	v4 := NewVertex(4)
+	v5.Connect(v4, 2, 200)
+	v10 := NewVertex(10)
+	v4.Connect(v10, 2, 700)
+	v8 := NewVertex(8)
+	v4.Connect(v8, 2, 300)
+	v8.Connect(v5, 2, 30)
+	v3 := NewVertex(3)
+	v3.Connect(v5, 2, 20)
+	v7 := NewVertex(7)
+	v7.Connect(v8, 2, 3)
+	v3.Connect(v7, 2, 40)
+	v1 := NewVertex(1)
+	v1.Connect(v7, 1, 6)
+	v1.Connect(v5, 2, 4)
 
+	g := &Graph{start: v5}
+	fmt.Print(g)
+	for _, v := range v5.FindAllPath(7) {
+		fmt.Println(v)
+	}
 }
